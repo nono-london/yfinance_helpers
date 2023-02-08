03290973314dae5cb8fb0e194a07f50026112281
thinkcentre <mysqlprojet@gmail.com>
2023-02-08 22:35:07 +0000

    THINKCENTRE: update main with packager of external libs

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+from yfinance_helpers.app_config import pack_python_libs_in_path
+
+pack_python_libs_in_path()
+
 from yfinance_helpers.yf_company_info.yf_options import update_ib_options_chain
 from multiprocessing import freeze_support
 
 
 def main():
+    pack_python_libs_in_path()
     update_ib_options_chain()
 
 
